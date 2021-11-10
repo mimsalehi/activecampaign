cbbcdc5e1abbd8e441fca9726c7203295ea706ed
Masoud Salehi <mimsalehi1991@gmail.com>
2021-11-10 13:58:44 +0330

    Add Contact list to ActiveCampaign api v3

---
+++ license.md
@@ -0,0 +1,5 @@
+# The license
+
+Copyright (c) Masoud Salehi <mimsalehi1991@gmail.comm>
+
+...Add your license text here...
---
+++ src/ActiveCampaign.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace MimSalehi\ActiveCampaign;
+
+/**
+ * Class ActiveCampaign
+ *
+ * @package MimSalehi\ActiveCampaign
+ */
+class ActiveCampaign
+{
+    // Build wonderful things
+
+    public function __construct(array $config)
+    {
+
+    }
+
+}
